65e8aff85fafdd32a84c31e93bf1974b580f302b
kamilltusinski <kamill.tusinski@gmail.com>
2024-12-13 15:29:58 +0100

    added others tests for the buttons on the iot dasboard page

--- page-object/iOTDashboardPage.ts
+++ page-object/iOTDashboardPage.ts
@@ -16,11 +16,59 @@ export class IOTDashboardPage  {
         await lightOffOnButton.click();
         const buttonStatus = this.page.locator('nb-card.off', {hasText:"Light"})
         if(await buttonStatus.isVisible()){
-            const textLightButtonOff = this.page.locator('.status.paragraph-2')
-            await expect(textLightButtonOff.first()).toContainText('OFF') 
+            const textLightButtonOff = lightOffOnButton.locator('.status.paragraph-2')
+            await expect(textLightButtonOff).toContainText('OFF') 
         } else {
-            const textLightButtonOn = this.page.locator('.status.paragraph-2')
-            await expect(textLightButtonOn.first()).toContainText('ON') 
+            const textLightButtonOn = lightOffOnButton.locator('.status.paragraph-2')
+            await expect(textLightButtonOn).toContainText('ON') 
+        }
+    }
+
+    /**
+     * This method turns off/on the roller shades button and checks if the text equals OFF or ON
+     */
+    async turnsOffOnTheRollerShades() {
+        const rollerShadesOffOnButton = this.page.locator('nb-card', {hasText:"Roller Shades"})
+        await rollerShadesOffOnButton.click()
+        const buttonStatus = this.page.locator('nb-card.off', {hasText:"Roller Shades"})
+        if(await buttonStatus.isVisible()) {
+            const textRollerShadesButtonOff = rollerShadesOffOnButton.locator('.status.paragraph-2')
+            await expect(textRollerShadesButtonOff).toContainText('OFF')
+        } else {
+            const textRollerShadesButtonOn = rollerShadesOffOnButton.locator('.status.paragraph-2')
+            await expect(textRollerShadesButtonOn).toContainText('ON')
+        }
+    }
+
+    /**
+     * This method turns off/on the wireless audio button and checks if the text equels OFF or ON
+     */
+    async turnsOffOnTheWirelessAudio(){
+        const wirelessAudioOffOnButton = await this.page.locator('nb-card',{hasText:"Wireless Audio"})
+        await wirelessAudioOffOnButton.click()
+        const buttonStatus = this.page.locator('nb-card.off',{hasText:"Wireless Audio"})
+        if(await buttonStatus.isVisible()){
+            const textWirelessAudioButtonOff = wirelessAudioOffOnButton.locator('.status.paragraph-2')
+            await expect(textWirelessAudioButtonOff).toContainText('OFF')
+        } else {
+            const textWirelessAudioButtonOn = wirelessAudioOffOnButton.locator('.status.paragraph-2')
+            await expect(textWirelessAudioButtonOn).toContainText('ON')
+        }
+    }
+
+      /**
+     * This method turns off/on the coffee maker button and checks if the text equels OFF or ON
+     */
+      async turnsOffOnTheCoffeeMaker(){
+        const coffeeMakerOffOnButton = await this.page.locator('nb-card',{hasText:"Coffee Maker"})
+        await coffeeMakerOffOnButton.click()
+        const buttonStatus = this.page.locator('nb-card.off',{hasText:"Coffee Maker"})
+        if(await buttonStatus.isVisible()){
+            const textCoffeeMakerButtonOff = coffeeMakerOffOnButton.locator('.status.paragraph-2')
+            await expect(textCoffeeMakerButtonOff).toContainText('OFF')
+        } else {
+            const textCoffeeMakerButtonOn = coffeeMakerOffOnButton.locator('.status.paragraph-2')
+            await expect(textCoffeeMakerButtonOn).toContainText('ON')
         }
     }
 }
--- tests/iOTDashboard.spec.ts
+++ tests/iOTDashboard.spec.ts
@@ -5,8 +5,27 @@ test.beforeEach( async({page}) => {
     await page.goto('/')
 })
 
-test('click on the light button', async({page}) => { 
+test('turns off/on the light button', async({page}) => { 
     const pm = new PageManager(page)
     await pm.onIOTDashboardPage().turnsOffOnTheLightButton()
     await pm.onIOTDashboardPage().turnsOffOnTheLightButton()
 })  
+
+test('turns off/on the roller shades button', async({page}) =>{
+    const pm = new PageManager(page)
+    await pm.onIOTDashboardPage().turnsOffOnTheRollerShades()
+    await pm.onIOTDashboardPage().turnsOffOnTheRollerShades()
+
+})
+
+test('turns off/on the wireless audio button', async({page}) =>{
+    const pm = new PageManager(page)
+    await pm.onIOTDashboardPage().turnsOffOnTheWirelessAudio()
+    await pm.onIOTDashboardPage().turnsOffOnTheWirelessAudio()
+})
+
+test('turns off/on the coffee maker button', async({page}) =>{
+    const pm = new PageManager(page)
+    await pm.onIOTDashboardPage().turnsOffOnTheCoffeeMaker()
+    await pm.onIOTDashboardPage().turnsOffOnTheCoffeeMaker()
+})
